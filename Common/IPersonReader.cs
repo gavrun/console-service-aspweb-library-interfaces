f27cf02f2b233c187aaba7620f4e7792888d6833
gavrun <38539655+gavrun@users.noreply.github.com>
2022-04-27 11:25:03 +0300

    added Common and Library

---
+++ Common/IPersonReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IPersonReader
+    {
+        //what for?
+        IEnumerable<Person> GetPeople();
+        
+        //get person id
+        Person GetPerson(int id);
+    }
+}
---
+++ Library/PersonReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class PersonReader
+    {
+        //service initializer ? how does it work?
+
+        #region Service initializers
+        WebClient client = new WebClient();
+        string baseUri = "http://localhost:9874";
+        JsonSerializerOptions options =
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        #endregion
+
+        public List<Person> GetPeople();
+
+
+        public Person GetPerson();
+
+
+
+
+    }
+}
